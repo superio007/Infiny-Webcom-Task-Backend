f25bbe8e9eff523e1ce5345711861a4642f2a856
Kiran GIG <dhokekiran88@gmail.com>
2025-12-31 00:51:30 +0530

    nnn

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import multer from "multer";
-import axios from "axios";
-import FormData from "form-data";
-import { PDFDocument } from "pdf-lib";
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const multer = require("multer");
+const axios = require("axios");
+const FormData = require("form-data");
+const { PDFDocument } = require("pdf-lib");
 
 dotenv.config();
 
